1f37fabc2331d614b93fb7107b8440968a882e0c
Javier <javier.dom.mart@gmail.com>
2018-06-02 22:04:05 +0100

    asdasd

--- checkRightWeights.py
+++ checkRightWeights.py
@@ -10,12 +10,16 @@ import numpy as np
 path= '/home/javier/Documents/CODIGOTITANBUENO/CodeInsideTitan/allWeights/'
 convo = np.load('/home/javier/Documents/CODIGOTITANBUENO/CodeInsideTitan/laputasalidadelaconvo.npy')
 convor = convo.reshape ((1,150))
-for i in range(120):
-    File  = path + 'Output'+ str(i) + '.npy'
-    outputFlatten = np.load(File)
-    substraction = convor - outputFlatten
-    print(sum(sum(substraction)))
-    
+for m in range(120):
+    for n in range(120):
+        File  = path + 'Output'+ str(m) +'_'+str(n)+ '.npy'
+        outputFlatten = np.load(File)
+        substraction = convor - outputFlatten
+        
+        if outputFlatten[0][1]==convor[0][1]:
+            print (m,n,sum(sum(substraction)))
+
+
     
     
 
